d1c0bf8f17f578dff219b6bc74f8783fe0cae525
Yuri <yuri.rampazo@outlook.com>
2021-04-29 12:36:22 -0300

    Rodape inicio

--- FrontEnd/eRural/src/app/app.module.ts
+++ FrontEnd/eRural/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
