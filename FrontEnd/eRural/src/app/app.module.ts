dd0fbca2e4ad48235b3c4ac9ba4f933ed56e11df
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2021-04-29 14:30:38 -0300

    componente menu

--- FrontEnd/eRural/src/app/app.module.ts
+++ FrontEnd/eRural/src/app/app.module.ts
@@ -4,12 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-<<<<<<< HEAD
-
-@NgModule({
-  declarations: [
-    AppComponent
-=======
 import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
 
@@ -18,7 +12,6 @@ import { MenuComponent } from './menu/menu.component';
     AppComponent,
     RodapeComponent,
     MenuComponent
->>>>>>> 1c5097af869fddcb95239d5dfdc048cdc67671eb
   ],
   imports: [
     BrowserModule,
