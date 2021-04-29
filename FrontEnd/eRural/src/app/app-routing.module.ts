d58c3506f7b0885f4d876df393604f1b5caffa9e
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2021-04-29 17:59:36 -0300

    rotas e comentarios ok

--- FrontEnd/eRural/src/app/app-routing.module.ts
+++ FrontEnd/eRural/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicialComponent } from './inicial/inicial.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+{path: "", redirectTo: 'inicio', pathMatch: 'full' },
 
 {path: 'login', component: LoginComponent},
-{path: 'cadastro', component: CadastroComponent}
+{path: 'cadastro', component: CadastroComponent},
+{path: 'inicio', component: InicialComponent}
 ];
 
 @NgModule({
