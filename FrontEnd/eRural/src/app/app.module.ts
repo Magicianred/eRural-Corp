74f96c916e668a6b940fa897d9ed6d68886726b8
Jaquelinecavalaro <jaqueline.cavalaro@gmail.com>
2021-04-29 15:47:14 -0300

    componente inicial

--- FrontEnd/eRural/src/app/app.module.ts
+++ FrontEnd/eRural/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { MenuComponent } from './menu/menu.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicialComponent } from './inicial/inicial.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
     RodapeComponent,
     MenuComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    InicialComponent
   ],
   imports: [
     BrowserModule,
