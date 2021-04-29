240fd2a942054c347e2459fec73a078c516fce44
Palmira Filipe <palmirafilipe24@gmail.com>
2021-04-29 12:21:39 -0300

    implementação de componente menu

--- FrontEnd/eRural/src/app/app.module.ts
+++ FrontEnd/eRural/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
